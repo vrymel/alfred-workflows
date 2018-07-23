ff1e7f8468e81a49e1059b62fe25d4187b48dfd7
Vrymel <vrymel.dev@gmail.com>
2018-07-23 13:13:27 +0800

    Added stimediff logic

--- stimediff/main.py
+++ stimediff/main.py
@@ -1,8 +1,13 @@
 import sys
+from datetime import datetime
 
 query = sys.argv[1]
 segments = query.split(" ")
 start_time = segments[0]
 end_time = segments[1]
+format = '%H%M'
 
-sys.stdout.write("{} - {}".format(start_time, end_time))
+timedelta = datetime.strptime(end_time, format) - datetime.strptime(start_time, format)
+hours_interval = timedelta.total_seconds() / 60 / 60
+
+sys.stdout.write(str(hours_interval))
